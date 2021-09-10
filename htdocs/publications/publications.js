cb796010dc628207587e54825feb627e49967c85
Luskyy <67589526+Luskyy@users.noreply.github.com>
2021-09-10 18:57:54 +0800

    Commit 2

--- htdocs/publications/publications.js
+++ htdocs/publications/publications.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ba6bb20b921b9a64fb4ba6a81615682653a88390988ee9e1fae65844e057f7f0
-size 2486
+oid sha256:7ba065212031a2865591dbd4fd242c8413bb084c45cadde4dcd9b60305b2b5e3
+size 11301
